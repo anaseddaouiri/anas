a65c018a6a7f8560f450b70cb66287372ba9c51e
anaseddaouiri <eddaouiri09@gmail.com>
2024-11-29 15:29:21 +0100

    jouncode
    vivacoding

---
+++ script.js
@@ -0,0 +1,17 @@
+// Sample Team Data
+const teams = [
+    { name: 'Real Madrid', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Real_Madrid_CF_logo.svg/1200px-Real_Madrid_CF_logo.svg.png' },
+    { name: 'FC Barcelona', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/FC_Barcelona_logo.svg/1200px-FC_Barcelona_logo.svg.png' },
+    { name: 'Atletico Madrid', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Atletico_Madrid_logo.svg/1200px-Atletico_Madrid_logo.svg.png' },
+    { name: 'Sevilla FC', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Sevilla_FC_logo.svg/1200px-Sevilla_FC_logo.svg.png' },
+    // Add more teams here...
+];
+
+// Sample Match Data
+const matches = [
+    { home: 'Real Madrid', away: 'Barcelona', date: '2024-12-01' },
+    { home: 'Atletico Madrid', away: 'Sevilla', date: '2024-12-02' },
+    // Add more matches here...
+];
+
+// Render Teams
